92588d93207489b264a30d165a7a2a95f275e859
RobinovichMSK <prokhorovk.v.94@yandex.ru>
2022-06-27 21:28:28 +0300

    ArrayHome.java Zadanie 1 done)

--- src/Page1/ArrayHome.java
+++ src/Page1/ArrayHome.java
@@ -1,4 +1,5 @@
 package Page1;
+
 import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicIntegerArray;
@@ -8,38 +9,54 @@ public class ArrayHome {
         Random rand = new Random();
         Scanner sc = new Scanner(System.in);
         int[] n = new int[sc.nextInt()];
-        for (int i = 0; i <n.length; i++) {
-            n[i]= rand.nextInt(10);
+        for (int i = 0; i < n.length; i++) {
+            n[i] = rand.nextInt(11);
         }
-        for (Integer array: n) {
-            System.out.println(array);
-            }
+        for (Integer array : n) {
+            System.out.print(array + " ");
+        }
         System.out.println("Dlina massiva " + n.length);
         System.out.println();
 
-        for (Integer number : n) {
-            if (number > 8){
-                for (int i =0; i>8; i++){
-                System.out.println("Kolvo >8 " + i );
-            }
+//       Количество чисел > 8 ?
+        int counter = 0;
+        for (Integer i : n) {
+            if (i > 8) {
+                counter = counter + 1;
             }
         }
+        System.out.println("Kolichestvo chisel > vosmi " + counter);
 
 
-//        System.out.println("kol-v0 chisel >8 : "+  хз что-то  );}
-//
-//
-//
-//        System.out.println();
-//        int sum =0;
-//        for (int n1: n) {
-//            sum = sum+n1;
-//        }
-//        System.out.println("Summa chisel massiva " + sum);
-//        System.out.println();
+        //       Количество чисел = 1
+        int counter2 = 0;
+        for (Integer i : n) {
+            if (i == 1) {
+                counter2 = counter2 + 1;
+            }
+        }
+        System.out.println("Kolichestvo chisel = odnomu " + counter2);
 
+        int chet = 0;
+        for (Integer i : n) {
+            if (i % 2 == 0) {
+                chet = chet + 1;
+            }
+        }
+        System.out.println("Kolichestvo chisel chetnih " + chet);
+        int nechet = 0;
+        for (Integer i : n) {
+            if (i % 2 != 0) {
+                nechet = nechet + 1;
+            }
+        }
+        System.out.println("Kolichestvo chisel ne chetnih " + nechet);
+        int sum = 0;
+        for (Integer i : n) {
+            sum = sum + i;
+        }
 
-
+        System.out.println("Summa chisel massiva =  " + sum);
 
 
     }
